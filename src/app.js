696a692aa94e1ce39cf5079809572058b9c1d4a5
annelisedomingz <annelisedomingz@gmail.com>
2021-06-15 20:55:28 -0400

    Filter Functionality integrated with API

--- src/app.js
+++ src/app.js
@@ -1,12 +1,50 @@
 function search(gifName) {
   let apiKey = "6IRRJvAoorEwG3Guy4NX48OEeeW5ZxQf";
-  let apiUrl = `https://api.giphy.com/v1/gifs/search?=api_key=${apiKey}&q=${gifName}`;
+  let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${gifName}&rating=g`;
 
-  axios.get(apiUrl).then(showGif);
+  axios.get(apiUrl).then(renderGif);
 }
 
-function showGif(gifData) {
-  console.log(gifData);
+// filterbutton click event
+
+let filterButtons = document.getElementsByClassName("filterButton");
+
+let handleClick = function () {
+  let genre = this.innerHTML;
+  let genreToSearch = `${genre} movie`;
+  search(genreToSearch);
+};
+
+for (let i = 0; i < filterButtons.length; i++) {
+  filterButtons[i].addEventListener("click", handleClick, false);
 }
 
-search("Aliens");
+// Response to Genre Button
+
+function renderGif(response) {
+  console.log(response);
+  let gifHTML = `<div class="row">`;
+  let gifElement = document.querySelector("#gif-results");
+  for (let i = 0; i < response.data.data.length; i++) {
+    let currentGif = response.data.data[i].images.fixed_height.url;
+    let currentGif_width = response.data.data[i].images.fixed_height.width;
+    let currentGif_height = response.data.data[i].images.fixed_height.height;
+
+    gifHTML =
+      gifHTML +
+      `
+<img src="${currentGif}" style="max-width: ${currentGif_width}px;max-height: ${currentGif_height}px;" />
+        
+      `;
+  }
+  gifHTML = gifHTML + `</div>`;
+  gifElement.innerHTML = gifHTML;
+}
+
+//
+
+function searchCityInput(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-field");
+  search(searchInput.value);
+}
